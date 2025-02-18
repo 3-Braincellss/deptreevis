8c3f17849fd8926e78140a728c8580952202df4f
sudoPom <sudopom@github.com>
2025-02-18 23:48:08 +0000

    File knows how to read itself

--- src/dep_tree_vis_file.rs
+++ src/dep_tree_vis_file.rs
@@ -1,5 +1,7 @@
+use std::fs;
 use std::path::PathBuf;
 
+#[derive(Debug)]
 pub struct DepTreeVisFile {
     path: PathBuf,
     children: Vec<Box<DepTreeVisFile>>,
@@ -11,5 +13,14 @@ impl DepTreeVisFile {
             path: PathBuf::from(path),
             children: Vec::new(),
         }
+    }
+
+    pub fn read(self) -> String {
+        fs::read_to_string(
+            self.path
+                .to_str()
+                .expect("This path is not utf-8 friendly :("),
+        )
+        .expect("This path doesnt exist.")
     }
 }
