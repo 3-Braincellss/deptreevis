16f2cc9ca14e2c80336a460fbff2dc1cf6562c0a
sudoPom <sudopom@github.com>
2025-02-19 00:21:12 +0000

    Remove main body

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,4 @@
 pub mod dep_tree_vis_file;
 pub mod walker;
 
-use dep_tree_vis_file::DepTreeVisFile;
-use walker::Walker;
-
-use crate::walker::python_walker::PythonWalker;
-
-fn main() {
-    let walker = PythonWalker::new("snippets/snip_python_0/run.py");
-    let file = DepTreeVisFile::new("src/walker/python_walker.rs");
-    println!("{}", file.read());
-    println!("{}", walker.walk());
-}
+fn main() {}
