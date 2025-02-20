f89f2477d51375551b1e420b5c4ac62c1b392a27
Oluwaponmile Femi-Sunmaila <72107751+sudoPom@users.noreply.github.com>
2025-02-20 00:08:21 +0000

    Walker dispatching (#11)
    Create a walker based on the file extension provided by the user.
    
    ---------
    
    Co-authored-by: sudoPom <sudopom@github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,38 @@
+use std::env;
+
+use walker::{python_walker::PythonWalker, rust_walker::RustWalker, Walker};
 pub mod file_node;
 pub mod walker;
 
-fn main() {}
+enum FileExtension {
+    Python,
+    Rust,
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    assert!(
+        args.iter().count() == 2,
+        "Usage: ./deptreevis <PROJECT-PATH>"
+    );
+    let path = args.get(1).unwrap();
+    let walker: Box<dyn Walker> = file_extension_to_walker(path);
+    println!("{:?}", walker.walk());
+}
+
+fn get_file_extension(path: &str) -> FileExtension {
+    let extension = path.split(".").last().unwrap();
+    return match extension {
+        "py" => FileExtension::Python,
+        "rs" => FileExtension::Rust,
+        _ => panic!("Unsupported extension, {}", extension),
+    };
+}
+
+fn file_extension_to_walker(path: &str) -> Box<dyn Walker> {
+    let extension = get_file_extension(path);
+    return match extension {
+        FileExtension::Python => Box::new(PythonWalker::new(path)),
+        FileExtension::Rust => Box::new(RustWalker::new(path)),
+    };
+}
