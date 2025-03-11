08f60c6100110bbaab61debc28837bcca2c786c9
Adi Bozzhanov <bozzhanov.at@gmail.com>
2025-03-11 23:06:52 +0000

    graph rendering mvp

--- clients/web/dpv-ui/src/pages/home.tsx
+++ clients/web/dpv-ui/src/pages/home.tsx
@@ -1,10 +1,26 @@
-import { Center, Title } from "@mantine/core";
+import ForceGraph2D from "react-force-graph-2d";
+import { Center, Stack, Title } from "@mantine/core";
+
+export function genRandomTree(N = 300, reverse = false) {
+  return {
+    nodes: [...Array(N).keys()].map((i) => ({ id: i })),
+    links: [...Array(N).keys()]
+      .filter((id) => id)
+      .map((id) => ({
+        [reverse ? "target" : "source"]: id,
+        [reverse ? "source" : "target"]: Math.round(Math.random() * (id - 1)),
+      })),
+  };
+}
 
 function Home() {
   return (
-    <Center>
-      <Title variant="h1">DPV-ui</Title>
-    </Center>
+    <Stack>
+      <Center>
+        <Title variant="h1">DPV-ui</Title>
+      </Center>
+      <ForceGraph2D graphData={genRandomTree(50)} />
+    </Stack>
   );
 }
 
